31d00bfa3ea57411769ac750adeec60cacdb2071
hamitdurmus <55110331+hamitdurmus@users.noreply.github.com>
2020-07-21 22:45:02 +0300

    Update gban.py

--- stdplugins/gban.py
+++ stdplugins/gban.py
@@ -26,39 +26,39 @@ async def _(event):
         if r.forward:
             r_from_id = r.forward.from_id or r.from_id
         else:
-             r_from_id = r.from_id
-         await borg.send_message(
-             -1001312478637,
-             "!fban {} {}".format(r.from_id, reason)
-         )
-     else:
-         user_id = event.pattern_match.group(1)
-         await borg.send_message(
-             -1001312478637,
-             "!fban {}".format(user_id)
-         )
-     await event.delete()
+            r_from_id = r.from_id
+        await borg.send_message(
+            -1001312478637,
+            "!fban {} {}".format(r.from_id, reason)
+        )
+    else:
+        user_id = event.pattern_match.group(1)
+        await borg.send_message(
+            -1001312478637,
+            "!fban {}".format(user_id)
+        )
+    await event.delete()
 
 
- @borg.on(admin_cmd(pattern="unfban ?(.*)"))
- async def _(event):
-     if Config.G_BAN_LOGGER_GROUP is None:
-         await event.edit("ENV VAR is not set. This module will not work.")
-         return
-     if event.fwd_from:
-         return
-     reason = event.pattern_match.group(1)
-     if event.reply_to_msg_id:
-         r = await event.get_reply_message()
-         r_from_id = r.from_id
-         await borg.send_message(
-             -1001312478637,
-             "!unfban {} {}".format(r_from_id, reason)
-         )
-     else:
-         user_id = event.pattern_match.group(1)
-         await borg.send_message(
-             -1001312478637,
-             "!unfban {}".format(user_id)
-         )
-  await event.delete()
+@borg.on(admin_cmd(pattern="unfban ?(.*)"))
+async def _(event):
+    if Config.G_BAN_LOGGER_GROUP is None:
+        await event.edit("ENV VAR is not set. This module will not work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        r_from_id = r.from_id
+        await borg.send_message(
+            -1001312478637,
+            "!unfban {} {}".format(r_from_id, reason)
+        )
+    else:
+        user_id = event.pattern_match.group(1)
+        await borg.send_message(
+            -1001312478637,
+            "!unfban {}".format(user_id)
+        )
+    await event.delete()
