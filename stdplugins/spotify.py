ed80ac7f81dba533aba5afe04b5bd4e58c9e1fd8
unknown <byazade@yandex.com>
2020-08-05 12:28:10 +0300

    redefining name 'save' from outer scope (line 174)

--- stdplugins/spotify.py
+++ stdplugins/spotify.py
@@ -54,8 +54,8 @@ async def set_biostgraph(event):
             "grant_type": "authorization_code", "redirect_uri": "https://example.com/callback",
             "code": Config.SPOTIFY_INITIAL_TOKEN}
     r = requests.post("https://accounts.spotify.com/api/token", data=body)
-    save = r.json()
-    to_create = {'bio': "test", 'access_token': save['access_token'], 'refresh_token': save['refresh_token'],
+    save_json = r.json()
+    to_create = {'bio': "test", 'access_token': save_json['access_token'], 'refresh_token': save_json['refresh_token'],
                  'telegram_spam': False, 'spotify_spam': False}
     with open('./database.json', 'w') as outfile:
         json.dump(to_create, outfile, indent=4, sort_keys=True)
