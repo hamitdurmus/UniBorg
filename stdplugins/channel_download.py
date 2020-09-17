da4f7e889af73d046f587ffc5a7f36ced463bba0
unknown <byazade@yandex.com>
2020-09-17 20:59:32 +0300

    code cleanup

--- stdplugins/channel_download.py
+++ stdplugins/channel_download.py
@@ -8,13 +8,15 @@ import asyncio
 import logging
 import os
 import subprocess
-
-from telethon import events
+from uniborg.util import admin_cmd
+from telethon import errors, events
 from telethon.errors import FloodWaitError
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 logger = logging.getLogger(__name__)
+
+directory = "./temp/"
 
 
 @borg.on(events.NewMessage(pattern=r"\.getc", outgoing=True))
@@ -49,37 +51,24 @@ async def get_media(event):
     await event.edit("Downloaded "+output+" files.")
 
 
-@borg.on(events.NewMessage(pattern=r"\.geta", outgoing=True))
+@borg.on(admin_cmd(pattern="geta ?(.*)", allow_sudo=True))
 async def get_media(event):
     if event.fwd_from:
         return
-    dir = "./temp/"
+    count = 0
+    os.makedirs(directory, exist_ok=True)
+    channel = event.pattern_match.group(1)
     try:
-        os.makedirs("./temp/")
-    except:
+        channel = int(channel)
+    except ValueError:
         pass
-    channel_username = event.text
-    command = ['ls', 'temp', '|', 'wc', '-l']
-    channel_username = channel_username[7:]
-
-    print(channel_username)
     await event.edit("Downloading All Media From this Channel.")
-    msgs = await borg.get_messages(channel_username, limit=3000)
-    with open('log.txt', 'w') as f:
-        f.write(str(msgs))
+    msgs = await borg.get_messages(channel, limit=3000)
     for msg in msgs:
         if msg.media is not None:
             try:
-                await borg.download_media(
-                    msg, dir)
-            except FloodWaitError as e:
-                await asyncio.sleep(20)
-                # await borg.download_media(
-            #     msg,dir)
-    ps = subprocess.Popen(('ls', 'temp'), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
-    ps.wait()
-    output = str(output)
-    output = output.replace("b'", "")
-    output = output.replace("\n'", "")
-    await event.edit("Downloaded "+output+" files.")
+                await borg.download_media(msg, directory)
+                count += 1
+            except errors.FloodWaitError as e:
+                await asyncio.sleep(e.seconds)
+    await event.edit(f"Downloaded {count} files.")
