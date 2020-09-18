980347026d52414728346a69a9b0e06d1b4a18c6
unknown <byazade@yandex.com>
2020-09-19 00:00:27 +0300

    edit

--- stdplugins/deleted_msg_group.py
+++ stdplugins/deleted_msg_group.py
@@ -14,43 +14,37 @@ async def new_msg(event):
 async def handler(event):
     me = await event.client.get_me()
     me_id = me.id
-    grup = await event.client.get_entity(event.chat_id)
-    group_ismi = grup.title
-    if me_id == from_id[0]:
-        return
-    else:
+    if not (me_id == from_id[0]):
+        grup = await event.client.get_entity(event.chat_id)
+        group_ismi = grup.title
         try:
             if event:
-
                 events = await event.client.get_admin_log(event.chat_id, delete=True)
                 user = await event.client.get_entity(events[0].user_id)
                 if not user.bot:
                     ismi = user.first_name
                     # if user.username is not None:
                     #     k_adi = user.username
-                    if not events[0].old.action:
-                        silinen_msg = events[0].old.message
-                        kullanici = f"[{ismi}](tg://user?id={user.id})"
-                        msg = f"**{group_ismi} Grubundan Silinen Mesaj\n\n**"\
-                            f"**Silen Kişi:** __{kullanici}__\n\n**Silinen Mesaj:** __{silinen_msg}__"
+                    # medya_down = await event.client.download_media(medya)
+                    silinen_msg = events[0].old.message
 
-                        if events[0].old.media is not None:
-
-                            medya = events[0].old.media
-                            msg = f"**{group_ismi} Grubundan Silinen Medya\n\n**"\
-                                f"**Silen Kişi:** __{kullanici}__\n\n"
-
-                            await event.client.send_message(
-                                entity=-1001220834298,
-                                message=msg,
-                                file=medya,
-                                force_document=False
-                            )
-                        else:
-
-                            await event.client.send_message(
-                                entity=-1001220834298,
-                                message=msg
-                            )
+                    kullanici = f"[{ismi}](tg://user?id={user.id})"
+                    msg = f"**{group_ismi} Grubundan Silinen Mesaj\n\n**"\
+                        f"**Silen Kişi:** __{kullanici}__\n\n**Silinen Mesaj:** __{silinen_msg}__"
+                    if events[0].old.media is not None:
+                        medya = events[0].old.media
+                        msg = f"**{group_ismi} Grubundan Silinen Medya\n\n**"\
+                            f"**Silen Kişi:** __{kullanici}__\n\n"
+                        await event.client.send_message(
+                            entity=-1001220834298,
+                            message=msg,
+                            file=medya,
+                            force_document=False
+                        )
+                    else:
+                        await event.client.send_message(
+                            entity=-1001220834298,
+                            message=msg
+                        )
         except ChatAdminRequiredError:
             return
