679f17670eea2c586feec81341480dd721b99997
unknown <byazade@yandex.com>
2020-09-22 21:49:55 +0300

    edit

--- stdplugins/youtube_downloader.py
+++ stdplugins/youtube_downloader.py
@@ -224,7 +224,6 @@ async def download_video(v_url):
                 progress(d, t, v_url, c_time, "Uploading..",
                          f"{ytdl_data['title']}.mp3")))
         # os.remove(file_path)
-        shutil.rmtree(out_folder)
         await asyncio.sleep(DELETE_TIMEOUT)
         os.remove(thumb_image)
         await j.delete()
